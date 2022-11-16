0c101f3e96f0614a606ccb55e0b090dd02d29027
Don0903 <78375532+Don0903@users.noreply.github.com>
2022-11-16 15:40:13 +0100

    Create THESIS_ML.R

---
+++ THESIS_ML.R
@@ -0,0 +1,630 @@
+####PACKAGES AND LIBRARY ####
+install.packages("rlang")
+install.packages("caret")
+install.packages("randomForest")
+install.packages("glmnet")
+install.packages("stringr")
+install.packages('caTools')
+install.packages("devtools")
+install.packages("tictoc")
+install.packages('varImp')
+#varimp
+install.packages("vip")
+library(vip)
+
+library(caTools)
+library("stringr")
+library(caret)
+library(tidyverse)
+library("readxl")
+library(tictoc)
+library(glmnet)
+library(dplyr)
+#for changing rownames to a column
+library(tibble)
+library(ranger)
+library(gbm)
+####PACKAGES END####
+
+set.seed(5)
+
+####DATA PREPARATION ####
+
+## LOAD THE DATA ##
+lipgene_data <- read.csv("C:\\Users\\hoken\\surfdrive\\Shared\\Akiya_Hoken_2022\\LIPGENE data\\LIPGENE_glucmap_semi.csv",
+                         header = T,
+                         dec= ".",
+                         sep = ",",
+                         stringsAsFactors = F,
+                         na.strings ="#LEEG!"
+)
+
+#Delete any duplicates, or rows containing no information (all and almost all NA's, which is dropout of 2,4,5,6)
+# duplicated(lipgene_data) #found no duplicates
+lipgene_data  <- filter(lipgene_data, !(lipgene_data$dropout %in% c(2,4,5,6)))
+
+#Eliminate useless data (we eliminate "diet" since we have "dietgroup" with same info) (SI_sqrt_ch is nzv, thus we eliminate)
+eliminate <- c("phase","partnernr","center","subjectnr","diet","MF_LF","dropout","SI_sqrt_ch")
+
+lipgene_data <- lipgene_data[, !(names(lipgene_data) %in% eliminate)] 
+####DATAPREP END ####
+
+#one-hot-encoding, make column male n female 1 yes 0 no
+#### PRE-PROCESSING ########
+#attempt for pre processing the entire data set (we only found one that had nzv)
+# nzv <- nearZeroVar(lipgene_data)
+# filteredLipgene <- lipgene_data[,-nzv]
+
+imp <- preProcess(lipgene_data, method = "bagImpute", k=5)
+lipgene_data <- predict(imp,lipgene_data)
+
+#save point to not keep running same 
+save <- lipgene_data
+lipgene_data <- save
+
+#Function for creating a column with their skeletal muscle mass
+lipgene_data$smm_delta <- with(lipgene_data, 
+                            lipgene_data$weight_ch*0.23
+                              )
+
+#get rid of weight related 
+eliminate_weights <- c("weight_pre","weight_mid","weight_ch","weightch_mid","weight_post","BMI_pre","BMI_post","BMI_ch","BMI_ln_ch")
+lipgene_data <- lipgene_data[, !(names(lipgene_data) %in% eliminate_weights)] 
+
+#now for checking for correlated predictors
+lipgeneCor <- cor(lipgene_data)
+summary(lipgeneCor[upper.tri(lipgeneCor)])  #highest has 0.99755 correlation, which is bad!! thus we get rid of them
+highcor <- findCorrelation(lipgeneCor, names=TRUE, cutoff = .85)
+print(highcor)
+# lipgene_data <- lipgene_data[,-highcor]
+# idk <- cor(lipgene_data)
+# summary(idk[upper.tri(idk)]) #now down to 160 variables!!
+
+#attempt to delete pre/post as reduction in dimension?
+# lipgene_data <- lipgene_data %>% select(-(contains("_ch")|(contains("_ln_")|contains("_kg_")|contains("_ltr_")|contains("_mean")|contains("_sqrt_")|contains("WHR")|contains("HOMA")|contains("LBM")|contains("PUFA_PCT_P"))))
+lipgene_data <- lipgene_data %>% select(-(contains(
+  c("_ch","_ln_","_kg_","_ltr_","_mean","_sqrt_","WHR","HOMA","LBM","PUFA_PCT_P","_post","TRL_TG_pre","_sin_","Work_index","Sports_index","Leisuretime_index","Total_physical_activity","apoB48_pre"))))
+
+#### PREPROCESSING END####
+
+#### DATA SPLITTING ####
+
+#create empty dataframes (for each type of diet)
+diet_A <- data.frame()
+diet_A_M <- data.frame()
+diet_A_F <- data.frame()
+diet_B <- data.frame()
+diet_B_M <- data.frame()
+diet_B_F <- data.frame()
+diet_C <- data.frame()
+diet_C_M <- data.frame()
+diet_C_F <- data.frame()
+diet_D <- data.frame()
+diet_D_M <- data.frame()
+diet_D_F <- data.frame()
+
+
+
+#split each diet groups and their sex (categorical)
+#VERY INEFFICIENT CODE FOR NOW!!!!
+for (i in 1:nrow(lipgene_data)) {
+  if (lipgene_data[i,]$dietgroup == 1){
+    diet_A = rbind(diet_A,lipgene_data[i,])
+    if (lipgene_data[i,]$gender==0){
+      diet_A_M = rbind(diet_A_M,lipgene_data[i,])
+    }
+    else{
+      diet_A_F = rbind(diet_A_F,lipgene_data[i,])
+    }
+  }
+  else if (lipgene_data[i,]$dietgroup == 2){
+    diet_B = rbind(diet_B,lipgene_data[i,])
+    if (lipgene_data[i,]$gender==0){
+      diet_B_M = rbind(diet_B_M,lipgene_data[i,])
+    }
+    else{
+      diet_B_F = rbind(diet_B_F,lipgene_data[i,])
+    }
+  }
+  else if (lipgene_data[i,]$dietgroup == 3){
+    diet_C = rbind(diet_C,lipgene_data[i,])
+    if (lipgene_data[i,]$gender==0){
+      diet_C_M = rbind(diet_C_M,lipgene_data[i,])
+    }
+    else{
+      diet_C_F = rbind(diet_C_F,lipgene_data[i,])
+    }
+  }
+  else if (lipgene_data[i,]$dietgroup == 4){
+    diet_D = rbind(diet_D,lipgene_data[i,])
+    if (lipgene_data[i,]$gender==0){
+      diet_D_M = rbind(diet_D_M,lipgene_data[i,])
+    }
+    else{
+      diet_D_F = rbind(diet_D_F,lipgene_data[i,])
+    }
+  }
+}
+
+#now remove info on diet groups and sex
+eliminate_ds <- c("gender","dietgroup")
+# AGAGIN, ABSOLUTELY HORRIFIC LOOKING CODE BUT ITS THERE FOR NOW
+diet_A <- diet_A[,!(names(diet_A) %in% eliminate_ds)]
+diet_A_F <- diet_A_F[, !(names(diet_A_F) %in% eliminate_ds)] 
+diet_A_M <- diet_A_M[, !(names(diet_A_M) %in% eliminate_ds)]
+diet_B <- diet_B[,!(names(diet_B) %in% eliminate_ds)]
+diet_B_F <- diet_B_F[, !(names(diet_B_F) %in% eliminate_ds)] 
+diet_B_M <- diet_B_M[, !(names(diet_B_M) %in% eliminate_ds)] 
+diet_C <- diet_C[,!(names(diet_C) %in% eliminate_ds)]
+diet_C_F <- diet_C_F[, !(names(diet_C_F) %in% eliminate_ds)] 
+diet_C_M <- diet_C_M[, !(names(diet_C_M) %in% eliminate_ds)]
+diet_D <- diet_D[,!(names(diet_D) %in% eliminate_ds)]
+diet_D_F <- diet_D_F[, !(names(diet_D_F) %in% eliminate_ds)] 
+diet_D_M <- diet_D_M[, !(names(diet_D_M) %in% eliminate_ds)] 
+
+# apparently this doesnt work neither :(
+# diets = list(diet_A,diet_A_F,diet_A_M,diet_B,diet_B_F,diet_B_M,
+#              diet_C,diet_C_F,diet_C_M,diet_D,diet_D_F,diet_D_M)
+# 
+# eliminateDS <- function(diets,variables){
+#   diets <- diets[,!(names(diets) %in% variables)]
+# }
+# 
+# for(i in 1:length(diets)){
+#   diets[[i]] = eliminateDS(diets[[i]],eliminate_ds)
+# }
+####DATASPLIT END####
+
+#### SPLIT IS ACTUALLY SO SHIT ########
+# diets = split(lipgene_data, f = lipgene_data$dietgroup)
+# diet_A<- as.data.frame(diets[1])
+# diet_A_g <- split(diet_A,f = diet_A$X1.gender)
+# diet_A_M <- as.data.frame(diet_A_g[1])
+####SPLIT end####
+
+####MODELRUN####
+
+#function for the models
+model_predict <- function(diet,model_name){
+  tic()
+  #reproducibility 
+  set.seed(5)
+ 
+  # Get the number of observations
+  n_obs <- nrow(diet)
+  
+  # Identify row to split on: split
+  split <- round(n_obs * 0.75)
+  
+  # Create train
+  train <- diet[1:split, ]
+  
+  # Create test
+  test <- diet[(split+1):nrow(diet),]
+  
+  #split predictor variable to target variable, favourable with caret packages
+  x <- train[,colnames(train) != "smm_delta"]
+  y <- train$smm_delta
+  
+  #model attempt
+  model <- train(
+    x = x,
+    y = y,
+    preProcess = c("center","scale"),
+    method = model_name,
+    
+    # #for ranger varImp
+    # importance = 'impurity',
+    
+    trControl = trainControl(
+      method =  "cv", 
+      number = 10,
+      verboseIter = TRUE
+    )
+  )
+  
+  predicted <- predict(model, test)
+  print(predicted)
+  
+  plot(predicted, test$smm_delta, main = model_name)
+  abline(reg=lm(test$smm_delta ~ predicted))
+  toc()
+  return(model)
+  
+}
+
+#create model objects that return model
+glmnet <- model_predict(diet_A,"glmnet")
+ranger <- model_predict(diet_D,"ranger")
+lm <- model_predict(diet_D,"lm")
+gbm <- model_predict(diet_D,"gbm")
+brnn <- model_predict(diet_D,"brnn")
+rf <- model_predict(diet_D,"rf")
+#gradient decent --> doesnt exist anymore, great!!
+# mlpSGD <- model_predict(diet_D,"mlpSGD")
+xgbLinear <- model_predict(diet_D,"xgbLinear")
+xgbTree<- model_predict(diet_D,"xgbTree")
+####MODEL RUN END####
+
+#--- MODEL ATTEMPTS ----
+
+  set.seed(5)
+  
+  # Get the number of observations
+  n_obs <- nrow(diet_A)
+  
+  # Identify row to split on: split
+  split <- round(n_obs * 0.750)
+  
+  # Create train
+  train <- diet_A[1:split, ]
+  
+  # Create test
+  test = diet_A[(split+1):nrow(diet_A),]
+  
+  #split predictor variable to target variable, favourable with caret packages
+  x <- train[,colnames(train) != "smm_delta"]
+  y <- train$smm_delta
+  
+
+
+####GLMNET ATTEMPT####
+set.seed(5)
+# custom tuning grid
+myGrid <- expand.grid(
+  alpha = seq(0,1, length =5),   #0 - pure ridge to 1- pure lasso.
+  lambda = seq(1,0.0001,length =100) #go from 0.0001 to 1, having 100 stops in between
+)
+
+#model attempt
+tic()
+glmnet <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "glmnet",
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+#create model object
+p_glmnet <- predict(glmnet, test)
+#plot the test data against the predicted, with best fit line
+plot(p_glmnet, test$smm_delta, main = "glmnet")
+abline(reg=lm(test$smm_delta ~ p_glmnet))
+toc()
+#out-ofsample rmse
+rmse_glmnet <- postResample(pred = p_glmnet, obs = test$smm_delta)
+####GLMNET END####
+
+####PLS ATTEMPT####
+set.seed(5)
+# custom tuning grid
+myGrid <- expand.grid(
+  ncomp = seq(1,5,length=10) #number of components to include in the model
+)
+
+#model attempt
+tic()
+pls <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "pls",
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+#create model object
+p_pls <- predict(pls, test)
+#plot the test data against the predicted, with best fit line
+plot(p_pls, test$smm_delta, main = "pls")
+abline(reg=lm(test$smm_delta ~ p_pls))
+toc()
+#out-ofsample rmse
+rmse_pls <- postResample(pred = p_pls, obs = test$smm_delta)
+####PLS END####
+
+####gbm ATTEMPT####
+set.seed(5)
+#custom tuning grid
+myGrid <- expand.grid(
+  n.trees = seq(10,200, length = 50), #number of trees, from 10 to 200 having 50 stops
+  interaction.depth = seq(1,10,length=10), #complexity of the trees
+  shrinkage = 0.1,  #learning rate
+  n.minobsinnode = 10  #minimum number of training set samples in a node
+)
+
+#model attempt
+gbm <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"), 
+  method = "gbm",
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+
+p_gbm <- predict(gbm, test)
+plot(p_gbm, test$smm_delta, main = "gbm")
+abline(reg=lm(test$smm_delta ~ p_gbm))
+toc()
+rmse_gbm <- postResample(pred = p_gbm, obs = test$smm_delta) #out of sample rmse
+####gbm END####
+
+####ranger ATTEMPT ####
+set.seed(5)
+#custom tuning grid
+myGrid <- expand.grid(
+  mtry = c(2,3,4,5,10,20),   #Number of variables to possibly split at in each node. 
+  min.node.size = c(10,20),    #Minimal node size
+  splitrule = c("variance", "extratrees", "maxstat")   #Splitting rule
+)
+
+#model attempt
+ranger <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "ranger",
+  importance = 'impurity',  #this is required for ranger model's varImp to work (why? idk)
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+
+p_ranger <- predict(ranger, test)
+plot(p_ranger, test$smm_delta, main = "ranger")
+abline(reg=lm(test$smm_delta ~ p_ranger))
+toc()
+rmse_ranger <- postResample(pred = p_ranger, obs = test$smm_delta)
+####ranger END####
+
+####rf ATTEMPT ####
+set.seed(5)
+#custom tuning grid
+myGrid <- expand.grid(
+  mtry = c(2,3,4,5,10,20)   #Number of variables to possibly split at in each node. 
+)
+
+#model attempt
+rf <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "rf",
+   #this is required for ranger model's varImp to work (why? idk)
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+
+p_rf <- predict(rf, test)
+
+plot(p_rf, test$smm_delta, main = "rf")
+abline(reg=lm(test$smm_delta ~ p_rf))
+toc()
+rmse_rf <- postResample(pred = p_rf, obs = test$smm_delta)
+# print(min(rf$results[2]))
+
+####rfEND####
+
+####brnn ATTEMPT####
+# set.seed(5)
+# #custom tuning grid
+# myGrid <- expand.grid(
+#   neurons = c(2,3,4,5,10)   #Number of variables to possibly split at in each node. 
+# )
+# 
+# #model attempt
+# brnn <- train(
+#   x = x,
+#   y = y,
+#   preProcess = c("center","scale"),
+#   method = "brnn",
+#   tuneGrid = myGrid,
+#   trControl = trainControl(
+#     method =  "cv", 
+#     number = 10,
+#     verboseIter = TRUE
+#   )
+# )
+# 
+# p_brnn <- predict(brnn, test)
+# plot(p_brnn, test$smm_delta, main = "brnn")
+# abline(reg=lm(test$smm_delta ~ p_brnn))
+# toc()
+# rmse_brnn <- postResample(pred = p_brnn, obs = test$smm_delta)
+# # print(min(brnn$results[2]))
+
+####brnnEND####
+
+####enet ATTEMPT####
+set.seed(5)
+#custom tuning grid
+myGrid <- expand.grid(
+  lambda = c(1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0, 100.0),   #
+  fraction = seq(0,1, length = 100)   #fraction of ratio between L1/L2 pentalty
+)
+
+#model attempt
+tic()
+enet <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "enet",
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+
+p_enet <- predict(enet, test)
+plot(p_enet, test$smm_delta, main = "enet")
+abline(reg=lm(test$smm_delta ~ p_enet))
+toc()
+rmse_enet <- postResample(pred = p_enet, obs = test$smm_delta)
+# print(min(enet$results[3]))
+
+####enetEND####
+
+####bridge ATTEMPT####
+# set.seed(5)
+# #model attempt
+# bridge <- train(
+#   x = x,
+#   y = y,
+#   preProcess = c("center","scale"),
+#   method = "bridge",
+#   trControl = trainControl(
+#     method =  "cv", 
+#     number = 10,
+#     verboseIter = TRUE
+#   )
+# )
+# 
+# p_bridge <- predict(bridge, test)
+# 
+# plot(p_bridge, test$smm_delta, main = "bridge")
+# abline(reg=lm(test$smm_delta ~ p_bridge))
+# toc()
+# rmse_bridge <- postResample(pred = p_bridge, obs = test$smm_delta)
+# # print(min(bridge$results[2]))
+
+####bridgeEND####
+
+####treebag ATTEMPT####
+set.seed(5)
+#model attempt
+set.seed(5)
+treebag <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "treebag",
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+
+p_treebag <- predict(treebag, test)
+
+plot(p_treebag, test$smm_delta, main = "treebag")
+abline(reg=lm(test$smm_delta ~ p_treebag))
+toc()
+rmse_treebag <- postResample(pred = p_treebag, obs = test$smm_delta)
+# print(min(treebag$results[2])) 
+
+####treebagEND####
+
+#NNET DIDNT WORK!!! MAYBE THE TREE DID NOT FIND A NICE SPLIT?
+
+####nnet ATTEMPT####
+set.seed(5)
+#custom tuning grid
+myGrid <- expand.grid(
+  size = c(1,2,3),  #number of hidden layers
+  decay = c(1e-3, 1e-2, 1e-1, 0.0, 1.0)
+)
+
+#model attempt
+set.seed(5)
+tic()
+nnet <- train(
+  x = x,
+  y = y,
+  preProcess = c("center","scale"),
+  method = "nnet",
+  tuneGrid = myGrid,
+  trControl = trainControl(
+    method =  "cv", 
+    number = 10,
+    verboseIter = TRUE
+  )
+)
+
+p_nnet <- predict(nnet, test)
+
+plot(p_nnet, test$smm_delta, main = "nnet")
+abline(reg=lm(test$smm_delta ~ p_nnet))
+toc()
+rmse_nnet <- postResample(pred = p_nnet, obs = test$smm_delta)
+#print(min(nnet$results[3]))
+####nnetEND####
+
+#--- MODEL ATTEMPT END ----
+
+#adaptive resampling exapmle
+# fitControl <- trainControl(
+#   method = "adaptive_cv",
+#   number = 3,
+#   repeats =3,
+#   adaptive = list(
+#     min = 2,   #minimum number of resamples per hyperparmeter (default at 5)
+#     alpha = 0.05,   #confidence level for removin g hyperparam (chanign this doenst influence that much)
+#     method = "gls",   #either "gls" or "BT" - resampling method. gls for linear models, or BT for models with large number of hyperparameters
+#     complete = TRUE   #when set to true, do we want to generate the resampling set?
+#   ),
+#   search = "random"
+# )
+
+
+####RESULTS####
+
+#get summary of comparison between each models, IN-SAMPLE RMSE'S 
+model_list <- list(glmnet,pls,gbm,ranger,rf,enet,treebag)
+results <- summary(resamples(model_list))
+#store the in-sample-rmse results, 
+is_rmse_results <- as.data.frame(results$statistics[2])
+#and rename the row titles to its respective model names
+rownames(is_rmse_results) <- c("glmnet" ,"pls","gbm","ranger","rf","enet", "treebag")
+is_rmse_results
+
+#OUT-OF-SAMPLE RMSE COMPARISON
+#store all out of sample rsme's as vector
+os_rmse <- c(rmse_glmnet[1],rmse_pls[1],rmse_gbm[1],rmse_ranger[1],rmse_rf[1],rmse_enet[1],rmse_treebag[1]) 
+#make df
+os <- data.frame(row.names = c("glmnet" ,"pls","gbm","ranger","rf","enet", "treebag"), os_rmse)
+#print the lowest rmse and its name
+print(os[which(os$os_rmse == min(os)),,drop=FALSE])
+
+#VARIABLE IMPORTANCE CALCULATIONS
+#apply varimp function to all elements of the model list
+list_varimp <- lapply(model_list,varImp,scale=FALSE)
+#store all this in one dataframe
+df_varimp <- data.frame(rows=43)
+for(i in 1:length(list_varimp)){
+  x <- list_varimp[[i]][1]
+  df_varimp <- cbind(df_varimp,x)
+}
+#remove weird column named "rows"
+df_varimp = df_varimp[,!names(df_varimp) %in% "rows"]
+colnames(df_varimp) <- c("glmnet" ,"pls","gbm","ranger","rf","enet", "treebag")
+#add the row numbers up to see the total importance? (maybe irrelevant)
+df_varimp = as.data.frame(scale(df_varimp))
+df_varimp$sum_scaled <- rowSums(df_varimp)
+
+
+
+#### RESULTS END ####                        
